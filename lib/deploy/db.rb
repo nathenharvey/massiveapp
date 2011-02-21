37618451d07e6838a32c857958389ad7521af961
Tom Copeland <tom@infoether.com>
2011-02-21 10:02:48 -0500

    FSE

--- lib/deploy/db.rb
+++ lib/deploy/db.rb
@@ -1,4 +1,4 @@
-namespace :deploy
+namespace :deploy do
   desc "Copy the database.yml file into current/"
   task :copy_in_database_yml do
     run "cp #{shared_path}/config/database.yml #{current_path}/config/"
