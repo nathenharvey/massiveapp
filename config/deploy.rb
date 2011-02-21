715c3b736d4a9168a26fd47ddf878fd0e77b3fc9
Tom Copeland <tom@infoether.com>
2011-02-21 09:41:53 -0500

    Example deploy file from cap1 chapter

---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+# START:require_and_application
+require 'bundler/capistrano'
+set :application, "massiveapp"
+# END:require_and_application
+# START:scm
+set :scm, :git
+set :repository, "."
+# END:scm
+# START:host
+server "localhost", :app, :db, :primary => true
+# END:host
+# START:ssh
+ssh_options[:port] = 2222
+ssh_options[:keys] = "/Library/Ruby/Gems/1.8/gems/vagrant-0.6.9/keys/vagrant"
+# END:ssh
+# START:user
+set :user, "vagrant"
+set :group, "vagrant"
+set :deploy_to, "/home/vagrant/massiveapp"
+set :use_sudo, false
+# END:user
+# START:copy_strategy
+set :deploy_via, :copy
+set :copy_strategy, :export
+# END:copy_strategy
+# START:passenger
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  desc "Restart the application"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+# END:passenger
